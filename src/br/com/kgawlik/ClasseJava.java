49df1268f467ed6e25428ed582176218bdb7c375
Karine <karinegawlik@hotmail.com>
2024-05-17 22:46:04 -0300

    commit inicial

---
+++ src/br/com/kgawlik/ClasseJava.java
@@ -0,0 +1,13 @@
+package br.com.kgawlik;
+
+public class ClasseJava {
+
+	public static void main(String[] args) {
+		String ambiente = args[0];
+		
+		if (ambiente.equals("Hello")) {
+			System.out.print("Hello World!!!");
+		}
+	}
+
+}
